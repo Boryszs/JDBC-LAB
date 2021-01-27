e2e0fe339352c83a90c2e1c997d0e44c19b06d09
Boryszs <12damie@gmail.com>
2021-01-27 02:55:12 +0100

    Clear GUI

--- src/main/java/jdbc/GUI/AdresGUI.java
+++ src/main/java/jdbc/GUI/AdresGUI.java
@@ -21,42 +21,14 @@ public class AdresGUI extends JFrame {
         JScrollPane scrollPane = new JScrollPane(table);
         JButton buttonBack = new JButton("<-");
         buttonBack.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        Label lMiejscowsc = new Label("Miejscowsc:");
-        TextField tMiejscowsc = new TextField();
-        tMiejscowsc.setPreferredSize(new Dimension(100, 20));
-        Label lUlica = new Label("Ulica:");
-        TextField tUlica = new TextField();
-        tUlica.setPreferredSize(new Dimension(100, 20));
-        Label lNrDomu = new Label("Nr Domu:");
-        TextField tNrDomu = new TextField();
-        tNrDomu.setPreferredSize(new Dimension(100, 20));
-        Label lKodPocztowy = new Label("Kod Pocztowy:");
-        TextField tKodPocztowy = new TextField();
-        tKodPocztowy.setPreferredSize(new Dimension(100, 20));
-        JButton buttonInsert = new JButton("Insert");
-        buttonInsert.setAlignmentX(Component.CENTER_ALIGNMENT);
-
         JPanel mainPanel = new JPanel();
         JPanel panelB = new JPanel();
-        JPanel panelBs = new JPanel();
-        panelBs.add(buttonInsert,BorderLayout.CENTER);
+
         panelB.setLayout(new BorderLayout());
         panelB.add(buttonBack, BorderLayout.WEST);
-        JPanel panelT = new JPanel();
-        panelT.add(lMiejscowsc);
-        panelT.add(tMiejscowsc);
-        panelT.add(lUlica);
-        panelT.add(tUlica);
-        panelT.add(lNrDomu);
-        panelT.add(tNrDomu);
-        panelT.add(lKodPocztowy);
-        panelT.add(tKodPocztowy);
         JPanel tablePanel = new JPanel();
         tablePanel.add(scrollPane);
         mainPanel.add(panelB);
-        mainPanel.add(panelT);
-        mainPanel.add(panelBs);
         mainPanel.add(tablePanel);
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         this.add(mainPanel);
--- src/main/java/jdbc/GUI/KlientGui.java
+++ src/main/java/jdbc/GUI/KlientGui.java
@@ -19,33 +19,16 @@ public class KlientGui extends JFrame{
         JTable table = new JTable(klientTableModel);
         table.setPreferredScrollableViewportSize(new Dimension(850,600));
         JScrollPane scrollPane = new JScrollPane(table);
-        Label lLogin = new Label("Login:");
-        TextField login = new TextField();
-        login.setPreferredSize(new Dimension(100,20));
-        Label lPassword = new Label("Password:");
-        TextField password = new TextField();
-        password.setPreferredSize(new Dimension(100,20));
         JButton buttonBack = new JButton("<-");
         buttonBack.setAlignmentX(Component.LEFT_ALIGNMENT);
-        JButton buttonInsert = new JButton("Insert");
-        buttonInsert.setAlignmentX(Component.CENTER_ALIGNMENT);
         JPanel panelB = new JPanel();
         panelB.setLayout(new BorderLayout());
-        JPanel panelF = new JPanel();
-        JPanel paneBs = new JPanel();
         JPanel mainPanel = new JPanel();
         JPanel panelT = new JPanel();
         panelB.add(buttonBack,BorderLayout.WEST);
-        paneBs.add(buttonInsert,BorderLayout.CENTER);
-        panelF.add(lLogin);
-        panelF.add(login);
-        panelF.add(lPassword);
-        panelF.add(password);
         panelT.add(scrollPane);
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.add(panelB);
-        mainPanel.add(panelF);
-        mainPanel.add(paneBs);
         mainPanel.add(panelT);
 
         buttonBack.addActionListener(new ActionListener() {
@@ -58,16 +41,6 @@ public class KlientGui extends JFrame{
                     }
                 });
                 dispose();
-            }
-        });
-
-
-        buttonInsert.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Klient.addKlient(login.getText(),password.getText());
-                klientTableModel.setKlienci(Klient.getKlient());
-                table.setModel(klientTableModel);
             }
         });
 
